444f224c92b4643996cd7ec8663f8901e1c0e543
Gilson123tenemea <tenemeaaguilar@gmail.com>
2024-04-21 18:58:38 -0500

    Primer commit

--- src/Interfases/Carreras.java
+++ src/Interfases/Carreras.java
@@ -29,7 +29,6 @@ public class Carreras extends javax.swing.JPanel {
     private void initComponents() {
 
         jTextField1 = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
@@ -37,15 +36,16 @@ public class Carreras extends javax.swing.JPanel {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jButton5 = new javax.swing.JButton();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jLabel4 = new javax.swing.JLabel();
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
         jButton8 = new javax.swing.JButton();
-        jSeparator1 = new javax.swing.JSeparator();
-        panelContenido = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jLabel16 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
 
         jTextField1.setText("jTextField1");
 
@@ -53,20 +53,15 @@ public class Carreras extends javax.swing.JPanel {
         setPreferredSize(new java.awt.Dimension(770, 500));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Ebrima", 1, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("CARRERAS EN OFERTA");
-        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 20, -1, -1));
-
         jLabel2.setForeground(new java.awt.Color(0, 0, 0));
         jLabel2.setText("Id:");
-        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 120, -1, -1));
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 100, -1, -1));
 
         jLabel3.setForeground(new java.awt.Color(0, 0, 0));
         jLabel3.setText("Descripcion:");
-        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 160, -1, -1));
-        add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 110, 180, -1));
-        add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 160, 180, -1));
+        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, -1, -1));
+        add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 100, 180, -1));
+        add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 140, 180, -1));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -87,13 +82,6 @@ public class Carreras extends javax.swing.JPanel {
         jButton5.setText("GUARDAR");
         add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 270, 130, 40));
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 110, 130, -1));
-
-        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel4.setText("Filtro de Busqueda");
-        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 110, -1, -1));
-
         jButton6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/CRUD/ELIMINAR.png"))); // NOI18N
         jButton6.setText("ELIMINAR");
         add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 270, 130, 40));
@@ -110,42 +98,35 @@ public class Carreras extends javax.swing.JPanel {
             }
         });
         add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 270, 130, 40));
-        add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 50, 230, 30));
 
-        panelContenido.setBackground(new java.awt.Color(255, 255, 255));
-        panelContenido.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanel4.setBackground(new java.awt.Color(51, 153, 255));
+        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel16.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        jLabel16.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel16.setText("CARRERAS");
+        jPanel4.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 0, -1, -1));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 10, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 480, Short.MAX_VALUE)
-        );
+        add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 770, 20));
 
-        panelContenido.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 14, 10, 480));
+        jLabel5.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel5.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel5.setText("Buscar por:");
+        add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 220, -1, -1));
 
-        jPanel2.setBackground(new java.awt.Color(0, 0, 0));
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "ID", "Descripcion", " " }));
+        add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 220, -1, -1));
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 10, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 480, Short.MAX_VALUE)
-        );
+        jLabel6.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel6.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel6.setText(":");
+        add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 220, 20, -1));
+        add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 220, 230, 20));
 
-        panelContenido.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 20, 10, 480));
-
-        add(panelContenido, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 770, 510));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/CRUD/Carreras.png"))); // NOI18N
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 50, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
@@ -160,17 +141,17 @@ public class Carreras extends javax.swing.JPanel {
     private javax.swing.JButton jButton8;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
-    private javax.swing.JPanel panelContenido;
+    private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
